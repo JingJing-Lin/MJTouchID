375470df724a687a21060ff9525b324b845914ec
YXCZ <minjing@mc2050.com>
2017-04-25 15:22:26 +0800

    Initial Commit

---
+++ MJTouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJTouchID
+//
+//  Created by YXCZ on 17/4/25.
+//  Copyright © 2017年 林民敬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
